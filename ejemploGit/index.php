2b74a93fb9b47aac2917eb7479cb27acdd8f67fc
david <you@example.com>
2020-04-22 13:24:17 +0200

    Se ha modificado el código

---
+++ ejemploGit/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejemplo de GIT para DAW</title>
+    </head>
+    <body>
+        <h1>Ejemplo de GIT</h1>
+    </body>
+</html>
